2a593ce26e0017d3c7b9dc793e12a01ad119d58e
hunguet231 <hungngv231@gmail.com>
2022-03-25 14:36:46 +0700

    improvement: re-structure components

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import {
-  createDrawerNavigator
-} from '@react-navigation/drawer';
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import 'react-native-gesture-handler';
-import Feedback from './components/common/Feedback';
-import Menu from './components/common/Menu';
-import Rate from './components/common/Rate';
-import Setting from './components/common/Setting/Setting';
-import Share from './components/common/Share';
-import Dashboad from './components/home/Dashboad';
 import { AppConstant } from './constants';
-import { Ionicons } from '@expo/vector-icons'; 
-import { AntDesign } from '@expo/vector-icons'; 
-
-const Drawer = createDrawerNavigator();
+import Drawer from './components/common/Drawer';
 
 export default function App() {
 
@@ -23,70 +11,7 @@ export default function App() {
     <NavigationContainer>
       <SafeAreaView style={styles.statusBar}>
       </SafeAreaView>
-      <Drawer.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: AppConstant.DEFAULT_APP_COLOR,
-          },
-          headerTitleStyle: {
-            color: '#fff',
-          },
-          headerTintColor: '#fff',
-        }}
-        initialRouteName="Trang chủ"
-        drawerContent={(props) => <Menu {...props} />}
-      >
-        <Drawer.Screen 
-          name="Trang chủ" 
-          component={Dashboad} 
-          options={{
-            title: 'Trang chủ',
-            drawerIcon: ({focused, size}) => (
-              <Ionicons name={`ios-home`} size={size} color={focused ? '#7092FE' : '#000'} />
-            ),
-         }}
-        />
-        <Drawer.Screen 
-          name="Chọn hạng bài thi" 
-          component={Setting} 
-          options={{
-            title: 'Chọn hạng bài thi',
-            drawerIcon: ({focused, size}) => (
-              <Ionicons name={`ios-settings`} size={size} color={focused ? '#7092FE' : '#000'} />
-            ),
-         }}
-        />
-        <Drawer.Screen 
-          name="Thông tin phản hồi" 
-          component={Feedback} 
-          options={{
-            title: 'Thông tin phản hồi',
-            drawerIcon: ({focused, size}) => (
-              <Ionicons name={`ios-mail`} size={size} color={focused ? '#7092FE' : '#000'} />
-            ),
-         }}  
-        />
-        <Drawer.Screen 
-          name="Chia sẻ ứng dụng" 
-          component={Share} 
-          options={{
-            title: 'Chia sẻ ứng dụng',
-            drawerIcon: ({focused, size}) => (
-              <Ionicons name={`ios-share-social`} size={size} color={focused ? '#7092FE' : '#000'} />
-            ),
-         }}  
-        />
-        <Drawer.Screen 
-          name="Đánh giá ứng dụng" 
-          component={Rate} 
-          options={{
-            title: 'Đánh giá ứng dụng',
-            drawerIcon: ({focused, size}) => (
-              <AntDesign name={`like1`} size={size} color={focused ? '#7092FE' : '#000'} />
-            ),
-         }}  
-        />
-      </Drawer.Navigator>
+      <Drawer/>
     </NavigationContainer>
   );
 }
---
+++ components/Test/Test.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import {View, StyleSheet} from "react-native"
+
+const Test = () => {
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test

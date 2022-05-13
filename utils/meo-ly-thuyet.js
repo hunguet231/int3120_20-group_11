edefeae7eea3f033c8738a1e8bf80c92e6933278
cnhung2001 <63276586+cnhung2001@users.noreply.github.com>
2022-05-13 13:02:35 +0700

    update content tips

--- utils/meo-ly-thuyet.js
+++ utils/meo-ly-thuyet.js
@@ -1,6 +1,3 @@
-// Hoan thien bo meo ly thuyet o day
-// Set 1: Tuong ung bo 'Khai niem va quy tac'
-// ...Tuong tu Set 2...
 export const MeoLyThuyet = [
   {
     set: 1,
@@ -12,27 +9,71 @@ export const MeoLyThuyet = [
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Câu khái niệm “Phương tiện giao thông thô sơ đường bộ” chọn  “Kể cả xe đạp máy”.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Câu khái niệm “Người điều khiển giao thông” là  “Cảnh sát giao thông”.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Câu khái niệm “Phần đường xe chạy” chọn câu không có chữ  “An toàn giao thông”.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Câu khái niệm “Làn đường” chọn câu có chữ “An toàn giao thông”.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Câu câu “Đỗ xe: không có giới hạn về thời gian và Dừng xe có giới hạn thời gian” chọn câu số 2.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Về nồng độ cồn, trong máu = 50, trong khi thở = 0,25 hoặc không có gì cả trong máu và khí thở.",
+      },
+      {
+        content: "Dừng xe, đỗ xe cách lề đường, hè phố không quá 0,25 mét.",
+      },
+      {
+        content: "Giữ khoảng cách 5 mét với đường sắt.",
+      },
+      {
+        content: "Sử dụng giấy phép lái xe giả cấm 05 năm.",
+      },
+      {
+        content:
+          "Chọn “Cơ quan quản lý giao thông” đối với các trường hợp Xe quá tải, xe quá khổ và xe vận chuyển hàng nguy hiểm.",
+      },
+      {
+        content:
+          "Chọn “UBNB cấp tỉnh” đối với các trường hợp cấm đi, cấm đồ, cấm dừng, đường ngược chiều.",
+      },
+      {
+        content:
+          "Loại giấy tờ mang theo: Giấy phép lái xe phù hợp với loại xe.",
+      },
+      {
+        content:
+          "Độ tuổi lấy bắng theo hạng (mỗi hạng cách nhau 3 tuổi): 16: Xe dưới 50cm3; 18: Hạng A, B; 21: Hạng C; 24: Hạng D, FC.; 27: Hạng E.",
+      },
+      {
+        content:
+          "Độ tuổi tối đa người lái xe ô tô hạng E: Nam 55 tuổi và nữ 50 tuổi.",
+      },
+      {
+        content: "Không lái xe liên tục quá 4 giờ.",
+      },
+      {
+        content:
+          "Gặp câu hỏi về hạng FE thì chọn Câu 1, hạng FC thì chọn Câu 2.",
+      },
+      {
+        content:
+          "Tốc độ giới hạn của xe: bên ngoài khu dân cư 80km/h < 3,5 tấn, 70 km/h > 3,5 tấn.",
+      },
+      {
+        content:
+          "Tốc độ của xe ô tô trên đường cao tốc, bạn lấy tốc độ (lớn nhất) trong câu hỏi rồi trừ cho 30 thì sẽ ra đáp án đúng.",
       },
     ],
   },
@@ -42,43 +83,14 @@ export const MeoLyThuyet = [
     data: [
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Đối với các câu có biển báo hiệu lệnh được đặt trước ngã ba, ngã tư. Chọn câu 1 nếu câu hỏi 1 dòng, câu 3 nếu câu hỏi có 2 dòng.",
+      },
+      {
+        content: "Biển báo cấm máy kéo thì không cấm ô tô tải và ngược lại.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Biển báo cấm rẽ trái thì cấm quay đầu nhưng biển báo cấm quay đầu thì không cấm rẽ trái.",
       },
     ],
   },
@@ -88,43 +100,33 @@ export const MeoLyThuyet = [
     data: [
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Đối với câu hỏi liên quan đến việc điều khiển của CSGT, CSGT giơ tay thẳng lên thì các loại xe phải đứng lại, đưa 1 hoặc 2 tay giang ngang thì xe trước và đằng sau phải đứng lại.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Đường giao nhau có vòng xuyến nhường bên trái, không có vòng xuyến nhường bên phải.",
+      },
+      {
+        content: "Thấy xuất hiện “Công an” chọn câu cố 3.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Xe nào đã vào ngã tư thì xe đó có quyền ưu tiên đi trước cao nhất.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Tiếp đó đến các xe ưu tiên. Trong các xe ưu tiên thì thứ tự là xe cứu hóa, xe quân sư, xe công an, xe cứu thương.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Tiếp đó nếu cùng là xe ưu tiên hoặc cùng là xe không ưu tiên thì xét đến đường ưu tiên, tức là xe nào nằm trên đường ưu tiên thì có quyền đi trước (Xem biển báo hoặc tín hiệu đèn).",
+      },
+      {
+        content: "Xe nào không vướng xe khác ở bên phải có quyền đi trước.",
       },
       {
         content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
-      },
-      {
-        content:
-          "Câu khái niệm “Phương tiện giao thông cơ giới đường bộ” chọn  “Kể cả xe máy điện”.",
+          "Thứ tự ưu tiên tiếp theo: xe rẽ phải -> xe đi thẳng -> xe rẽ trái.",
       },
     ],
   },
